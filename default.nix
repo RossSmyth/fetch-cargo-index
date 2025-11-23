154f26a14d0964f6d26c6060f52b1e9b2417f995
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-11-23 18:02:01 -0500

    Add openssl

--- default.nix
+++ default.nix
@@ -1,10 +1,22 @@
 {
   lib,
   rustPlatform,
+  pkg-config,
+  openssl,
+  curl,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "fetch-cargo-index";
   version = "none";
 
   cargoLock.lockFile = ./Cargo.lock;
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
+    curl
+  ];
 })
